98ceeeca3357542f56b8b9a40f03807541d50769
Jaskaran Singh <thecodersingh@gmail.com>
2024-09-14 13:54:20 -0700

    Create swipe functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import "./css/global.scss";
 import Dashboard from "./pages/Dashboard";
+import Discover from "./pages/Discover";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/discover" element={<Discover />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/Find.js
@@ -0,0 +1,111 @@
+import "../css/find.scss";
+
+import TinderCard from "react-tinder-card";
+import Erlich from "../assets/erlich.jpg";
+import Richard from "../assets/richard.jpg";
+import { createRef, useMemo, useRef, useState } from "react";
+
+const Find = () => {
+  const db = [
+    {
+      name: "Richard Hendricks",
+      url: Richard,
+    },
+    {
+      name: "Erlich Bachman",
+      url: Erlich,
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
+  const [lastDirection, setLastDirection] = useState();
+
+  const currentIndexRef = useRef(currentIndex);
+
+  const childRefs = useMemo(
+    () =>
+      Array(db.length)
+        .fill(0)
+        .map((i) => createRef()),
+    [db.length]
+  );
+
+  const updateCurrentIndex = (val) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
+  };
+
+  const swiped = (direction, index) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  };
+
+  const canSwipe = currentIndex >= 0;
+
+  const canGoBack = currentIndex < db.length - 1;
+
+  const swipe = async (dir) => {
+    if (canSwipe && currentIndex < db.length) {
+      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  };
+
+  return (
+    <div>
+      <h1>Discover</h1>
+      <div className="cardContainer">
+        {db.map((character, index) => (
+          <TinderCard
+            key={character.name}
+            ref={childRefs[index]}
+            className="swipe"
+            onSwipe={(dir) => swiped(dir, character.name, index)}
+            onCardLeftScreen={() => outOfFrame(character.name, index)}
+          >
+            <div
+              style={{ backgroundImage: "url(" + character.url + ")" }}
+              className="card"
+            >
+              <h3>{character.name}</h3>
+            </div>
+            <div className="buttons">
+              <button
+                style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+                onClick={() => swipe("left")}
+              >
+                Swipe left!
+              </button>
+              <button style={{ backgroundColor: !canGoBack && "#c3c4d3" }}>
+                Undo swipe!
+              </button>
+              <button
+                style={{ backgroundColor: !canGoBack && "#c3c4d3" }}
+                onClick={() => swipe("right")}
+              >
+                Swipe right!
+              </button>
+            </div>
+            {lastDirection ? (
+              <h2 key={lastDirection} className="infoText">
+                You swiped {lastDirection}
+              </h2>
+            ) : (
+              <h2 className="infoText">Swipe a card or press a button!</h2>
+            )}
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Find;
