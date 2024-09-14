98fc15cffb13d85f2cc270d59f9b0c00420f1c26
noufilsaqib <noufil.s@hotmail.com>
2024-09-14 13:02:17 -0700

    Scaffolded login and signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+
 import "./css/global.scss";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
     </Routes>
   );
 }
