1e443ed030acbd7ca3327d5386df06a158184c7b
shadmanhossain <shadmanh@sfu.ca>
2024-09-14 14:55:09 -0700

    Creating Discover Page

---
+++ frontend/src/pages/Discovery.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+const Discover = () => {
+  return (
+    <div className="discover-page">
+      <h1>Discover People</h1>
+    </div>
+  );
+};
+export default Discover;
