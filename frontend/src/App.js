343bf30323af0d4a03c8245a7df072d11f76132b
shadmanhossain <shadmanh@sfu.ca>
2024-09-14 14:35:39 -0700

    Fixed some conflicts in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import "./css/global.scss";
 import Dashboard from "./pages/Dashboard";
-<<<<<<< HEAD
-import Discover from "./pages/Discover";
-=======
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
->>>>>>> 98fc15cffb13d85f2cc270d59f9b0c00420f1c26
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
-<<<<<<< HEAD
-      <Route path="/discover" element={<Discover />} />
-=======
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<Signup />} />
->>>>>>> 98fc15cffb13d85f2cc270d59f9b0c00420f1c26
     </Routes>
   );
 }
